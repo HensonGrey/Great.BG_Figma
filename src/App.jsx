bdd60e836d056aeaad4002db0e76e9a00c03cb11
Valentin Mihailov <valiomihailov.mih@gmail.com>
2025-10-04 14:25:30 +0300

    moved the arrays in another file, to uncluster the page

--- src/App.jsx
+++ src/App.jsx
@@ -12,93 +12,23 @@ import {
   Award,
 } from "lucide-react";
 import earringsImage from "./assets/images/Rectangle 662.png";
-import alternativeEarringImage1 from "./assets/images/Rectangle 666.png";
-import alternativeEarringImage2 from "./assets/images/Rectangle 667.png";
-import alternativeEarringImage3 from "./assets/images/Rectangle 668.png";
-import alternativeEarringImage4 from "./assets/images/Rectangle 669.png";
-import similarProduct1 from "./assets/images/IMAGE.png";
-import similarProduct2 from "./assets/images/IMAGE1.png";
-import similarProduct3 from "./assets/images/IMAGE2.png";
-import similarProduct4 from "./assets/images/IMAGE3.png";
 import vectorArrow from "./assets/images/arrow.png";
 import vectorCurvedArrow from "./assets/images/curvedArrow.png";
 import Header from "./components/Header";
 import SizePicker from "./components/SizePicker";
 import LeftRightChevrons from "./components/LeftRightChevrons";
-import fakeRev1Img from "./assets/images/fakeRev1.png";
-import fakeRev2Img from "./assets/images/fakeRev2.png";
-import fakeRev3Img from "./assets/images/fakeRev3.png";
 import FakeReview from "./components/FakeReview";
 import { useCarousel } from "./hooks/useCarousel";
 
+import {
+  similarProducts,
+  fakeReviews,
+  alternativeImages,
+  colors,
+  sizes,
+} from "./data/mainPage";
+
 function App() {
-  const sizes = ["S", "M", "L", "XL", "XXL"];
-
-  const alternativeImages = [
-    { src: alternativeEarringImage1 },
-    { src: alternativeEarringImage2 },
-    { src: alternativeEarringImage3 },
-    { src: alternativeEarringImage4 },
-  ];
-
-  const similarProducts = [
-    {
-      src: similarProduct1,
-      title: "Smart Watch With Bracelet - Black",
-      price: "25,00 лв.",
-    },
-    {
-      src: similarProduct2,
-      title: `Ароматна свещ Flower bouquet`,
-      price: "25,00 лв.",
-    },
-    {
-      src: similarProduct3,
-      title: `Илюстрация Лястовица`,
-      price: "25,00 лв.",
-    },
-    {
-      src: similarProduct4,
-      title: `Книга с илюстрации Малкият принц`,
-      price: "$29.25",
-    },
-  ];
-
-  const colors = [
-    { bgColor: "#ECDECC" },
-    { bgColor: "#BBD278" },
-    { bgColor: "#BBC1F8" },
-    { bgColor: "#FFD3F8" },
-    { gradient: "#FFB6B6 50%, #98C185 50%" },
-  ];
-
-  const fakeReviews = [
-    {
-      title: "Страхотни бижута!",
-      review:
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-      posterPicture: fakeRev1Img,
-      posterName: "Sophia Moore",
-      posterRole: "CEO at Webflow Agency",
-    },
-    {
-      title: "Перфектните подаръци!",
-      review:
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-      posterPicture: fakeRev2Img,
-      posterName: "Adam Smith",
-      posterRole: "Webflow Developer",
-    },
-    {
-      title: "Супер качество на продуктите",
-      review:
-        "Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non",
-      posterPicture: fakeRev3Img,
-      posterName: "Mike Warren",
-      posterRole: "Developer at BRIX Templates",
-    },
-  ];
-
   const productCarousel = useCarousel(similarProducts);
   const reviewCarousel = useCarousel(fakeReviews);
 
---
+++ src/data/mainPage.js
@@ -0,0 +1,79 @@
+import alternativeEarringImage1 from "../assets/images/Rectangle 666.png";
+import alternativeEarringImage2 from "../assets/images/Rectangle 667.png";
+import alternativeEarringImage3 from "../assets/images/Rectangle 668.png";
+import alternativeEarringImage4 from "../assets/images/Rectangle 669.png";
+import similarProduct1 from "../assets/images/IMAGE.png";
+import similarProduct2 from "../assets/images/IMAGE1.png";
+import similarProduct3 from "../assets/images/IMAGE2.png";
+import similarProduct4 from "../assets/images/IMAGE3.png";
+
+import fakeRev1Img from "../assets/images/fakeRev1.png";
+import fakeRev2Img from "../assets/images/fakeRev2.png";
+import fakeRev3Img from "../assets/images/fakeRev3.png";
+
+export const sizes = ["S", "M", "L", "XL", "XXL"];
+
+export const alternativeImages = [
+  { src: alternativeEarringImage1 },
+  { src: alternativeEarringImage2 },
+  { src: alternativeEarringImage3 },
+  { src: alternativeEarringImage4 },
+];
+
+export const similarProducts = [
+  {
+    src: similarProduct1,
+    title: "Smart Watch With Bracelet - Black",
+    price: "25,00 лв.",
+  },
+  {
+    src: similarProduct2,
+    title: `Ароматна свещ Flower bouquet`,
+    price: "25,00 лв.",
+  },
+  {
+    src: similarProduct3,
+    title: `Илюстрация Лястовица`,
+    price: "25,00 лв.",
+  },
+  {
+    src: similarProduct4,
+    title: `Книга с илюстрации Малкият принц`,
+    price: "$29.25",
+  },
+];
+
+export const colors = [
+  { bgColor: "#ECDECC" },
+  { bgColor: "#BBD278" },
+  { bgColor: "#BBC1F8" },
+  { bgColor: "#FFD3F8" },
+  { gradient: "#FFB6B6 50%, #98C185 50%" },
+];
+
+export const fakeReviews = [
+  {
+    title: "Страхотни бижута!",
+    review:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    posterPicture: fakeRev1Img,
+    posterName: "Sophia Moore",
+    posterRole: "CEO at Webflow Agency",
+  },
+  {
+    title: "Перфектните подаръци!",
+    review:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    posterPicture: fakeRev2Img,
+    posterName: "Adam Smith",
+    posterRole: "Webflow Developer",
+  },
+  {
+    title: "Супер качество на продуктите",
+    review:
+      "Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non",
+    posterPicture: fakeRev3Img,
+    posterName: "Mike Warren",
+    posterRole: "Developer at BRIX Templates",
+  },
+];
